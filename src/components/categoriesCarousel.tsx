31dcc8d8f49a332d2650fcbc01fe5c493e4382d3
Abdul-Hannan232 <abdulhani232@gmail.com>
2024-06-03 15:56:27 +0500

    home page complete

--- src/components/categoriesCarousel.tsx
+++ src/components/categoriesCarousel.tsx
@@ -1,12 +1,11 @@
-import { View, Text, ImageBackground } from "react-native";
-
+import { View, Text, ImageBackground, Dimensions } from "react-native";
 export default function Category() {
     return <>
 
 
         <View style={{ padding: 33 }}>
             <Text style={{ fontSize: 22, color: "#2D3748", fontWeight: "bold" }}>Popular Categories</Text>
-            <View style={{display:"flex",flexDirection:"row",alignItems:"center",gap:10,marginTop:20}}>
+            <View style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 10, marginTop: 20 }}>
                 <View>
                     <ImageBackground source={require('./../../assets/images/beverages.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
 
@@ -14,35 +13,24 @@ export default function Category() {
                     </ImageBackground>
                 </View>
                 <View>
-                    <ImageBackground source={require('./../../assets/images/beverages.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
+                    <ImageBackground source={require('./../../assets/images/snacks.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
 
-                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Beverages</Text>
+                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Snacks</Text>
                     </ImageBackground>
                 </View>
                 <View>
-                    <ImageBackground source={require('./../../assets/images/beverages.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
+                    <ImageBackground source={require('./../../assets/images/sauces.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
 
-                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Beverages</Text>
+                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Sauces</Text>
                     </ImageBackground>
                 </View>
                 <View>
-                    <ImageBackground source={require('./../../assets/images/beverages.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
+                    <ImageBackground source={require('./../../assets/images/dairy.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
 
-                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Beverages</Text>
+                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Dairy</Text>
                     </ImageBackground>
                 </View>
-                <View>
-                    <ImageBackground source={require('./../../assets/images/beverages.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
 
-                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Beverages</Text>
-                    </ImageBackground>
-                </View>
-                <View>
-                    <ImageBackground source={require('./../../assets/images/beverages.png')} style={{ width: 100, height: 100, display: "flex", justifyContent: "flex-end", flexDirection: "column", alignItems: "center", paddingBottom: 10 }}>
-
-                        <Text style={{ color: "#FFFFFF", fontSize: 15, }}>Beverages</Text>
-                    </ImageBackground>
-                </View>
             </View>
         </View>
     </>
---
+++ src/components/searches.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { View, Text, ImageBackground, Image, StyleSheet } from "react-native";
+
+export default function Search() {
+    return (
+        <View style={styles.root}>
+            <View style={styles.container}>
+                <Text style={styles.title}>Popular Searches</Text>
+                <View style={{borderBottomColor:"gray",borderWidth:1,paddingBottom:30,borderRightColor:"transparent",borderLeftColor:"transparent",borderTopColor:"transparent"}}>
+                <View style={styles.row}>
+                    <View style={styles.card}>
+                        <ImageBackground
+                            source={require('./../../assets/images/cocacola.png')}
+                            style={styles.imageBackground}
+                        >
+                            <Image source={require('./../../assets/images/israel.png')} style={styles.image} />
+                        </ImageBackground>
+                        <Text style={styles.cardText}>Coca-Cola</Text>
+                    </View>
+                    <Image source={require('./../../assets/images/arrow.png')} style={{position:"absolute",left:147,zIndex:50}}/>
+                    <View style={styles.card}>
+                        <ImageBackground
+                            source={require('./../../assets/images/colanext.png')}
+                            style={styles.imageBackground}
+                        >
+                            <Image source={require('./../../assets/images/pak.png')} style={styles.image} />
+                        </ImageBackground>
+                        <Text style={styles.cardText}>Cola Next</Text>
+                    </View>
+                </View>
+                <Text style={{color:"#D21F3C",textAlign:"center",fontSize:15,fontWeight:"bold",marginTop:20}}> View All Alternatives</Text>
+                </View>
+                <View style={{borderBottomColor:"gray",borderWidth:1,paddingBottom:30,borderRightColor:"transparent",borderLeftColor:"transparent",borderTopColor:"transparent"}}>
+                <View style={styles.row}>
+                    <View style={styles.card}>
+                        <ImageBackground
+                            source={require('./../../assets/images/cocacola.png')}
+                            style={styles.imageBackground}
+                        >
+                            <Image source={require('./../../assets/images/israel.png')} style={styles.image} />
+                        </ImageBackground>
+                        <Text style={styles.cardText}>Coca-Cola</Text>
+                    </View>
+                    <Image source={require('./../../assets/images/arrow.png')} style={{position:"absolute",left:147,zIndex:50}}/>
+                    <View style={styles.card}>
+                        <ImageBackground
+                            source={require('./../../assets/images/colanext.png')}
+                            style={styles.imageBackground}
+                        >
+                            <Image source={require('./../../assets/images/pak.png')} style={styles.image} />
+                        </ImageBackground>
+                        <Text style={styles.cardText}>Cola Next</Text>
+                    </View>
+                </View>
+                <Text style={{color:"#D21F3C",textAlign:"center",fontSize:15,fontWeight:"bold",marginTop:20}}> View All Alternatives</Text>
+                </View>
+           
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+        
+    },
+    container: {
+        padding: 33,
+    },
+    title: {
+        fontSize: 22,
+        color: "#2D3748",
+        fontWeight: "bold",
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 20,
+        gap:20
+    },
+    card: {
+        borderColor: "gray",
+        padding: 8,
+        backgroundColor: "#F8F9FA",
+        height: 155,
+        borderWidth: 1,
+        borderRadius: 20,
+        marginRight: 10, // Added marginRight to simulate gap
+        zIndex:20
+    },
+    imageBackground: {
+        height: 110,
+        width: 140,
+    },
+    image: {
+        width: 50,
+        height: 50,
+    },
+    cardText: {
+        fontSize: 15,
+        color: "black",
+        fontWeight: "bold",
+        paddingLeft: 15,
+        paddingTop: 5,
+    },
+});
+
